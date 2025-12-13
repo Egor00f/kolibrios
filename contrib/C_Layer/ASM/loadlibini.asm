95c5a5eec67a770602f29266c2c3d1d85690ded2
Егор <148970641+Egor00f@users.noreply.github.com>
2025-12-14 01:07:40 +0500

    Update loadlibini.asm

--- contrib/C_Layer/ASM/loadlibini.asm
+++ contrib/C_Layer/ASM/loadlibini.asm
@@ -3,44 +3,47 @@ use32                                   ; Tell compiler to use 32 bit instructio
 	
 section '.flat' code			; Keep this line before includes or GCC messes up call addresses
 
-;include 'struct.inc'
+
 include '../../../programs/proc32.inc'
 include '../../../programs/macros.inc'
-purge section,mov,add,sub
+purge section, mov, add, sub
 
 include '../../../programs/dll.inc'
 	
-public init_libini as '_kolibri_libini_init'
+public  init_libini		as	'_kolibri_libini_init'
 	
 ;;; Returns 0 on success. -1 on failure.
 
-proc init_libini
+proc    init_libini
 	pusha
-	mcall 68,11
 	stdcall dll.Load, @IMPORT
 	popa
 	ret
-endp	
-	
+endp
+
 @IMPORT:
 
-library lib_libini,               'libini.obj'
+library libini,                 'libini.obj'
 
-import  lib_libini, \
-		LIBINI_enum_sections           , 'ini_enum_sections'    , \
-        LIBINI_enum_keys               , 'ini_enum_keys'        , \
-        LIBINI_get_str                 , 'ini_get_str'          , \
-        LIBINI_get_int  			   , 'ini_get_int'   		, \
-        LIBINI_get_color               , 'int_get_color'        , \
-        LIBINI_get_shortcut            , 'int_get_shortcut'     , \
-        LIBINI_set_str            	   , 'ini_set_str'          , \
-        LIBINI_set_int         		   , 'ini_set_int'          , \          
-        LIBINI_set_color          	   , 'ini_set_color'
+import  libini                               ,  \
+        LIBINI_enum_sections,   'ini_enum_sections',    \
+        LIBINI_enum_keys,       'ini_enum_keys',        \
+        LIBINI_get_int,         'ini_get_int',  \
+        LIBINI_get_str,         'ini_get_str',  \
+        LIBINI_get_color,       'ini_get_color',        \
+        LIBINI_set_str,         'ini_set_str',  \
+        LIBINI_set_int,         'ini_set_int',  \
+        LIBINI_set_color,       'ini_set_color',        \
+        LIBIN_get_shortcut,     'ini_get_shortcut'
         
-public LIBINI_enum_sections         as  '_LIBINI_enum_sections' 
-public LIBINI_enum_keys             as  '_LIBINI_enum_keys'     
-public LIBINI_get_str               as  '_LIBINI_get_str'       
-public LIBINI_get_int  			    as  '_LIBINI_get_int'  		
-public LIBINI_get_color             as  '_LIBINI_get_color'     
-public LIBINI_set_str            	as  '_LIBINI_set_str'       
-public LIBINI_set_int         		as  '_LIBINI_set_int'       
+
+
+public LIBINI_get_str		as	'_LIBINI_get_str'
+public LIBINI_get_int		as	'_LIBINI_get_int'
+public LIBINI_set_str		as	'_LIBINI_set_str'
+public LIBINI_set_int		as	'_LIBINI_set_int'
+public LIBINI_set_color		as	'_LIBINI_set_color'
+public LIBINI_get_color		as	'_LIBINI_get_color'
+public LIBINI_enum_sections     as      '_LIBINI_enum_sections'
+public LIBINI_enum_keys         as      '_LIBINI_enum_keys'
+public LIBIN_get_shortcut       as      '_LIBIN_get_shortcut'
